c1c5dfb82cc55ef8f9336d235329d822ffaf70b1
Ouassim001 <89780864+Ouassim001@users.noreply.github.com>
2022-02-04 09:51:04 +0100

    2nd commit

--- SnackBar2.py
+++ SnackBar2.py
@@ -1,9 +1,19 @@
+frites = "French Fries?\n"
+frikad1 = "Normal Frikadel?\n"
+frikad2 = "veggie Frikadel?\n"
+frikadel3 = "XXL Frikadel?\n"
+qroqBread = "Qroquette With Bread?\n"
+QroqNoBread = "Qroquette without bread?\n"
+
+
+
 def much():
-    amountfries = int(input("how many french fries?\n"))
-    amountfrikad1 = int(input("how many normal frikadel?\n"))
-    amountfrikad2 = int(input("how many veggie frikadel?\n"))
-    amountfrikad3 = int(input("how many XXL frikadel?\n"))
-    amountqroq = int(input("how many qroquette?\n"))
+    amountfries = int(input("how many " + frites))
+    amountfrikad1 = int(input("how many " + frikad1))
+    amountfrikad2 = int(input("how many " + frikad2))
+    amountfrikad3 = int(input("how many " + frikadel3))
+    amountqroq1 = int(input("how many " + qroqBread))
+    amountqroq2 = int(input("how many " + QroqNoBread))
     for i in range(amountfries):
         fritesauce = input("what sauce for the fries?\n")
     
@@ -16,8 +26,8 @@ def much():
     # for f in range(amountfrikad3):
     #     ofkind = input("what kind of frikadel?\n")
 
-    for k in range(amountqroq):
-        bread = input("with or without bread?\n")
+    # for k in range(amountqroq):
+    #     bread = input("with or without bread?\n")
 
     fritesauce = 0.20 * amountfries
     print(fritesauce)
@@ -31,8 +41,20 @@ def much():
     amountfrikad3 = 5.67 * amountfrikad3
     print(amountfrikad3)
 
-    amountqroq = 0.75 * amountqroq
-    print(amountqroq)
+    amountqroq1 = 0.75 * amountqroq1
+    print(amountqroq1)
+
+    amountqroq2 = 0.75 * amountqroq2
+    print(amountqroq2)
+
+    float(print(frites + "---" + fritesauce))
+
+    
+
+
+
+
+
 
 
 
